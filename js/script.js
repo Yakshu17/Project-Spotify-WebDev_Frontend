b0ddf950c4257b287a745d0edd74fd30747cb982
chandershekhar <yakshu17@gmail.com>
2022-10-08 11:13:55 +0530

    git init

--- js/script.js
+++ js/script.js
@@ -6,16 +6,25 @@ let audioElement =new Audio('songs/1.mp3');
 let masterplay= document.getElementById('masterplay');
 let myprogressbar= document.getElementById('myprogressbar');
 let gif= document.getElementById('gif');
-
+let songItem=Array.from(document.getElementsByClassName('songItem'));
 //Array or Objects for song lists
 let songs=[
     {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"},
-    {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"},
-    {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"},
-    {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"},
-    {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"},
-    {songName:"Let Me Love You",filepath:"songs/1.mp3",coverpath:"img/img1.jpg"}
+    {songName:"Lovely",filepath:"songs/2.mp3",coverpath:"img/img2.png"},
+    {songName:"Someone will love you better",filepath:"songs/3.mp3",coverpath:"img/img3.jpg"},
+    {songName:"Stay",filepath:"songs/4.mp3",coverpath:"img/img4.png"},
+    {songName:"Baila Conmigo",filepath:"songs/5.mp3",coverpath:"img/img5.jpg"},
+    {songName:"Let Me Down Slowly",filepath:"songs/6.mp3",coverpath:"img/img6.jpg"},
+    {songName:"Closer",filepath:"songs/7.mp3",coverpath:"img/img7.png"}
 ]
+
+songItem.forEach((element,i)=> {
+    console.log(element,i);
+ element.getElementsByTagName("img")[0].src=songs[i].coverpath; 
+ element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+
+    
+})
 
 // handle play/pause click
 masterplay.addEventListener('click',()=>{
